f92ea6e855a22f51bba428c4bd83c8c1a6f4cc12
insu Sonn <45266970+gds1110@users.noreply.github.com>
2025-01-02 21:50:55 +0900

    시작 알림 메시지 출력

--- src/com/sis/exam/board/Main.java
+++ src/com/sis/exam/board/Main.java
@@ -4,6 +4,10 @@ package com.sis.exam.board;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("== 자바 텍스트 게시판 ==");
+        System.out.println("== 프로그램 시작 ==");
+        System.out.println("명령)");
+        System.out.println("== 프로그램 종료 ==");
 
     }
 }
