fd800f871c01c20cce038739f7adcaccd63abae0
insu Sonn <45266970+gds1110@users.noreply.github.com>
2025-01-03 23:29:12 +0900

    feat: add write function
    #3

--- src/com/sis/exam/board/Main.java
+++ src/com/sis/exam/board/Main.java
@@ -11,16 +11,28 @@ public class Main {
         System.out.println("== 자바 텍스트 게시판 ==");
         System.out.println("== 프로그램 시작 ==");
 
-        while (true)
-        {
+        while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
             System.out.printf("입력 된 명령어 : %s\n", cmd);
 
-            if(cmd.equals("exit"))
-            {
+            if (cmd.equals("/user/article/write")) {
+                System.out.println("==게시물 등록==");
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+
+                System.out.printf("내용 : ");
+                String content =sc.nextLine();
+
+                int id = 1;
+                System.out.println(id+"번 게시물이 등록되었습니다.\n");
+            }
+            else if (cmd.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            }
+            else {
+                System.out.println("명령어를 잘못 입력하셨습니다.");
             }
         }
 
